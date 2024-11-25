a135c8c9422c37abe82b8481bbcaa54482e74219
NDMcCa <nicholas.m19@gmail.com>
2024-11-25 12:46:16 -0700

    Late commit: added currently incomplete components for the Moderator's page

---
+++ src/lib/components/BanListItem.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    export let title: string;
+    export let username: string;
+    export let mdusername: string;
+    export let reason: string;
+  </script>
+  
+  <div class="banned-user">
+    <h2>{title}</h2>
+    <p class="username">{username}</p>
+    <p class="username">Banned by: {mdusername}</p>
+    <p class="desc">Ban reason: {reason}</p>
+  </div>
+  
+  <style lang="banned-user">
+    .listing {
+      background-color: white;
+      color: black;
+      margin: 1rem;
+      margin-bottom: 0;
+      padding: 1rem;
+      width: 100%;
+    }
+  
+    h2 {
+      margin: 0;
+    }
+  
+    p.username {
+      margin: 0;
+      margin-top: 0.5rem;
+      font-size: 0.9rem;
+    }
+  
+    p.price {
+      margin: 0;
+      margin-top: 0.5rem;
+      font-size: 1.2rem;
+      color: rgb(37, 214, 37) !important;
+  
+      &::before {
+        content: "$";
+      }
+    }
+  
+    p.desc {
+      margin: 0;
+      margin-top: 1.5rem;
+    }
+  
+    :global(body.dark-mode) {
+      .listing {
+        background-color: #333;
+        color: white;
+      }
+  
+      h2 {
+        color: white;
+      }
+  
+      p {
+        color: white;
+      }
+    }
+  </style>
+  
