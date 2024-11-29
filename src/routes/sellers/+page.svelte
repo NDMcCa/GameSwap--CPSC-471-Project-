60b37fab3dc520ff80954f68526ba8bf3cde089b
Ryan Grewal <ryansgrewal@gmail.com>
2024-11-29 16:17:08 -0700

    Changed Port#, added /sellers + /sellers/{userId} routes
    - Changed the docker port number from 3306 to 4000
    
    - Added a route for /sellers to display the full list of sellers
    
    - Added a route for /sellers/{userID} to display information for each individual seller

---
+++ src/routes/sellers/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import "../../app.scss";
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+
+<main>
+    <Nav />
+
+    <div>
+        <h1>Sellers:</h1>
+    </div>
+</main>
---
+++ src/routes/sellers/[sellerId]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import "../../../app.scss";
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+
+<main>
+    <Nav />
+    <h1>This will show a specific user</h1>
+
+
+</main>
